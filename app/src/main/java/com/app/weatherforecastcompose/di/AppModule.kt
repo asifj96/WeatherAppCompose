86b2a162661079e0bce57d11c45a55fffb47dc2d
asifj96 <masifj1996@gmail.com>
2023-07-04 01:32:21 +0500

    Project Structure,Splash Screen,model classes

---
+++ app/src/main/java/com/app/weatherforecastcompose/di/AppModule.kt
@@ -0,0 +1,4 @@
+package com.app.weatherforecastcompose.di
+
+class AppModule {
+}
---
+++ app/src/main/java/com/app/weatherforecastcompose/navigation/WeatherScreens.kt
@@ -0,0 +1,5 @@
+package com.app.weatherforecastcompose.navigation
+
+enum class WeatherScreens {
+    SplashScreen, MainScreen, AboutScreen, FavoriteScreen, SearchScreen, SettingsScreen
+}
